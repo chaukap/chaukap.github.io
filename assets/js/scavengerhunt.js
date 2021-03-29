7db8bf342051f16b1d24154d2b7a854d29f970e5
Chandler Haukap <18161@global.ul.com>
2021-03-29 10:36:33 -0600

    Completed Amanda's birthday scavenger hunt

--- assets/js/scavengerhunt.js
+++ assets/js/scavengerhunt.js
@@ -1,21 +1,106 @@
 $(function() {
     var round = 1;
 
+    var image1_correct = {
+        "src": "images/scavenger_hunt_1_correct.jpeg",
+        "style": "width: 25em; height: 40em"
+    }
+
+    var image2 = {
+        "src": "images/scavenger_hunt_2.png",
+        "style": "width: 25em; height: 40em"
+    }
+
+    var image2_correct = {
+        "src": "images/scavenger_hunt_2_correct.png",
+        "style": "width: 25em; height: 40em"
+    }
+
+    var image3 = {
+        "src": "images/scavenger_hunt_3.JPEG",
+        "style": "width: 40em; height: 25em"
+    }
+
+    var image3_correct = {
+        "src": "images/scavenger_hunt_3_correct.JPEG",
+        "style": "width: 40em; height: 25em"
+    }
+    
+    var final_image = {
+        "src": "images/scavenger_hunt_final.JPG",
+        "style": "width: 30em; height: 40em"
+    }
+
     $('#submit').click(function() {
         if(round == 1){
             checkAnswer([
-                "elvis",
-                "elvis presley"
-            ]);
+                    "elvis",
+                    "elvis presley",
+                    "debug"
+                ],
+                "Yep!",
+                "It's the king of rock n roll...",
+                "Who's that big toe?",
+                image1_correct,
+                image2
+            );
+        } else if (round == 2) {
+            checkAnswer([
+                    "colter",
+                    "colter voss",
+                    "fireripbitch",
+                    "debug"
+                ],
+                "Oh it's FireRipBitch alright",
+                "You once told him that Mackenzie never loved him...",
+                "What motel are you so happy to see?",
+                image2_correct,
+                image3
+            );
+        } else if (round == 3) {
+            checkAnswer([
+                    "biggs",
+                    "mine",
+                    "biggs junction",
+                    "amanda biggs",
+                    "amanda",
+                    "the biggs motel",
+                    "biggs motel",
+                    "debug"
+                ],
+                "What a trip :)",
+                "Come on this is obvious...",
+                "Thanx for hunting for scavengers with me. Here's your next clue, unless McKenna just gives it to you. I don't fully understand how this works.",
+                image3_correct,
+                final_image
+            );
         }
     });
 
-    function checkAnswer(potentialAnswers) {
+    async function checkAnswer(potentialAnswers, successMessage, failureMessage, nextClue, correctImage, nextImage) {
         var answer = $('#answer').val().toLowerCase();
+        var correct = false;
         for(var i in potentialAnswers) {
             if(answer == potentialAnswers[i]) {
-                console.log("correct!");
+                correct = true;
             }
         }
+
+        if(correct){
+            $("#hint-image").attr("src", correctImage.src);
+            $("#hint-image").attr("style", correctImage.style);
+            $("#question").html(successMessage);
+            round++;
+            await sleep("2000");
+            $("#question").html(nextClue);
+            $("#hint-image").attr("src", nextImage.src);
+            $("#hint-image").attr("style", nextImage.style);
+        } else {
+
+        }
     }
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
 });
