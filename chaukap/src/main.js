606848f137d2eecb7edced320cd37e1c369b36f5
Chandler Haukap <18161@global.ul.com>
2021-06-18 11:15:56 -0600

    added Azure application insights

--- chaukap/src/main.js
+++ chaukap/src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
+
+const appInsights = new ApplicationInsights({ config: {
+  instrumentationKey: '2608f7ca-16c4-4d95-a308-6f3c1872c329'
+  /* ...Other Configuration Options... */
+} });
+appInsights.loadAppInsights();
+appInsights.trackPageView();
 
 createApp(App).mount('#app')
